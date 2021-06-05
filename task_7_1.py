46cd7bbad97c2a22d1bd9367c5575f805da4d132
Yuriy <kireev_juriyy@rambler.ru>
2021-06-05 11:46:36 +0800

    Complete task_7_1 / Add change in the programm

--- task_7_1.py
+++ task_7_1.py
@@ -1,10 +1,15 @@
 import os
+import shutil
 
 dir_first = 'my_project'
 dir_second = ['settings', 'mainapp', 'adminapp', 'authapp']
 if not os.path.exists(dir_first):
     for dir_odd in dir_second:
-        dir_path = os.path.join(dir_first, dir_odd)
-        os.makedirs(dir_path)
+        if not os.path.exists(dir_odd):
+            dir_path = os.path.join(dir_first, dir_odd)
+            os.makedirs(dir_path)
+        else:
+            shutil.copytree(dir_odd, os.path.join(dir_first, dir_odd))
+            shutil.rmtree(dir_odd)
 else:
     print('Каталог my_project уже существует')
